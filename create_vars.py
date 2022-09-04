4f2fffd6d3c2d6b9c1a15fd348fd51948deb9442
jbmahaffey <jbmahaffey@gmail.com>
2022-09-03 17:39:37 -0700

    update

--- create_vars.py
+++ create_vars.py
@@ -7,20 +7,38 @@ import json
 
 def main():
 
+# Create IP variables
     devices = []
-    with open(os.path.join(sys.path[0],'address.csv'), 'r') as vars_:
+    with open(os.path.join(sys.path[0],'ip_address.csv'), 'r') as vars_:
         for line in csv.DictReader(vars_):
             devices.append(line)
-    data = devices
 
-    with (open("vars.tf", "w")) as f:
-        f.write("variable \"test\" { \n  default = {")
+    with open("vars.tf", "w") as f:
+        f.write("variable \"test_ip\" { \n  default = {")
 
     with open('vars.tf', 'a') as f:
-        for i in data:
+        for i in devices:
+            f.write('\n    {} = '.format(i['name']))
+            json.dump(i, f, indent=6, separators=[",", " = "])
+        f.write('\n}\n}\n\n\n')
+
+# Create fqdn variables
+    fqdnlist = []
+    with open(os.path.join(sys.path[0],'fqdn_address.csv'), 'r') as vars_:
+        for line in csv.DictReader(vars_):
+            fqdnlist.append(line)
+
+    with open('vars.tf', 'a') as f:
+        f.write("variable \"test_fqdn\" { \n  default = {")
+
+    with open('vars.tf', 'a') as f:
+        for i in fqdnlist:
             f.write('\n    {} = '.format(i['name']))
             json.dump(i, f, indent=6, separators=[",", " = "])
         f.write('\n}\n}')
 
 if __name__ == '__main__':
    main()
+
+# May want to add if statement to search dictionary for group name and types and create terraform
+# variables based on these groups
--- vars.tf
+++ vars.tf
@@ -1,11 +1,10 @@
-variable "test" { 
+variable "test_ip" { 
   default = {
     test1 = {
       "name" = "test1",
       "type" = "ipmask",
       "subnet" = "1.1.1.0",
       "mask" = "255.255.255.0",
-      "fqdn" = "",
       "allow_routing" = "false"
 }
     test2 = {
@@ -13,7 +12,6 @@ variable "test" {
       "type" = "ipmask",
       "subnet" = "2.2.2.0",
       "mask" = "255.255.255.0",
-      "fqdn" = "",
       "allow_routing" = "false"
 }
     test3 = {
@@ -21,15 +19,24 @@ variable "test" {
       "type" = "ipmask",
       "subnet" = "3.3.3.3",
       "mask" = "255.255.255.255",
-      "fqdn" = "",
       "allow_routing" = "false"
 }
+}
+}
+
+
+variable "test_fqdn" { 
+  default = {
     msn = {
       "name" = "msn",
       "type" = "fqdn",
-      "subnet" = "",
-      "mask" = "",
       "fqdn" = "msn.com",
+      "allow_routing" = "false"
+}
+    hbo = {
+      "name" = "hbo",
+      "type" = "fqdn",
+      "fqdn" = "hbo.com",
       "allow_routing" = "false"
 }
 }
